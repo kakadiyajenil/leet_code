7533b3e2197008100d73cc9e4d8f8ab72aac3911
jenil kakadiya <jenilkakadiya2602@gmail.com>
2024-02-09 08:13:17 +0530

    9-2-24

---
+++ leet4.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn,memo={}) {
+    return function(...args) {
+    let id=JSON.stringify(args)
+    if(id in memo){
+    return memo[id]
+    }
+    let x=fn(...args)
+    memo[id]=x
+    return x;
+    
+    }
+    }
