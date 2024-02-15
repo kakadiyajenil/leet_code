4f969a8fef302c129d1af9c55c30bdea2435b235
jenil kakadiya <jenilkakadiya2602@gmail.com>
2024-02-15 08:06:35 +0530

    15-02-2024

--- leet4.js
+++ leet4.js
@@ -1,16 +1,19 @@
-/**
- * @param {Function} fn
- * @return {Function}
- */
-function memoize(fn,memo={}) {
-    return function(...args) {
-    let id=JSON.stringify(args)
-    if(id in memo){
-    return memo[id]
-    }
-    let x=fn(...args)
-    memo[id]=x
-    return x;
+// <==179==>
+var largestNumber = function(nums) {
+    const merge = (a, b) => {
+        const order1 = parseInt(${a}${b});
+        const order2 = parseInt(${b}${a});
+        return order2 - order1; 
+    };
     
+    const sortedNums = nums.map(String).sort(merge);
+    
+    if (sortedNums[0] === '0') {
+        return '0';
     }
-    }
+    
+    return sortedNums.join('');
+};
+
+const nums = [9,9,6,7,8,5,4,6,9,8,10];
+console.log(largestNumber(nums));
