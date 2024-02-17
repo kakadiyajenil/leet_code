4fdbbe5c4734b534bb686d3d850ca5fac3868058
jenil kakadiya <jenilkakadiya2602@gmail.com>
2024-02-17 08:07:03 +0530

    17-02-2024

--- leet5.js
+++ leet5.js
@@ -41,3 +41,13 @@ function memoize(fn,memo={}) {
 
 }
     }
+
+let callCount = 0;
+  const memoizedFn = memoize(function (a, b) {
+ 	 callCount += 1;
+    return a + b;
+  })
+  memoizedFn(2, 3) // 5
+  memoizedFn(2, 3) // 5
+  console.log(callCount) // 1 
+ 
